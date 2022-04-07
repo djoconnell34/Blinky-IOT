2aaa4e0cb6fd31e720e082f96d329d137b0cd032
Daniel O'Connell <djoconnell34@gmail.com>
2022-04-07 18:04:12 -0400

    Add project files.

---
+++ Blinky/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Device.Gpio;
+
+namespace Blinky
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello this is a IOT App");
+
+            int pin = 18;
+            GpioController controller = new GpioController();
+
+            controller.OpenPin(pin, PinMode.Output);
+
+            while (true)
+            {
+                controller.Write(pin, PinValue.High);
+                Thread.Sleep(1000);
+                controller.Write(pin, PinValue.Low);
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
